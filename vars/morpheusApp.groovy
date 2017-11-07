4204afcf32421ba64bfbdf29f43a4d0014e68a81
Adam Hicks <thomas.adam.hicks@gmail.com>
2017-11-06 19:52:17 -0700

    reversion

--- vars/morpheusApp.groovy
+++
@@ -1,6 +0,0 @@
-import org.incognitojoe.JenkinsHttpClient
-
-def buildApp(String morpheusUrl, String postBody, String headering) {
-        JenkinsHttpClient http = new JenkinsHttpClient()
-        http.postJson(morpheusUrl, postBody, headering)
-}
